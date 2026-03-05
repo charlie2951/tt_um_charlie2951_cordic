f0018b08636e0aa9d415c173c8105d13c91b81ba
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-03-05 11:50:37 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,7 +21,7 @@ parameter BAUD     = 115200;
   // All output pins must be assigned. If not used, assign to 0.
  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
  // assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_oe  = 8'hFF;
 
   // List all unused inputs to prevent warnings
     wire _unused = &{ena, ui_in[1],ui_in[2],ui_in[3],ui_in[4],ui_in[5],ui_in[6],ui_in[7],uio_in[0],uio_in[1],uio_in[2],uio_in[3],uio_in[4],uio_in[5],uio_in[6],uio_in[7], 1'b0};
